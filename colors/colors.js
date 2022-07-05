dcc10bde3ab2629f38399a4c312c76421e775d10
Jacob Shulenberger <jacob@builtdigital.io>
2022-07-05 16:42:24 -0500

    chore: update colors

--- colors/colors.js
+++ colors/colors.js
@@ -5,54 +5,83 @@
  *  Source: https://github.com/jacobshu/synthwave85
  ********************************************************************************/
 
-const sw85 = {
-    dark0_hard: '#191b26',
-    dark0: '#282a36',
-    dark0_soft: '#383a46',
-    dark1: '#484a57',
-    dark2: '#595b68',
-    dark3: '#6b6d7a',
-    dark4: '#7d7f8c',
-    dark4_256: '#7d7f8c',
-
-    gray_245: '#90929f',
-    gray_244: '#90929f',
-
-    light0_hard: '#ffffff',
-    light0: '#f9f9ff',
-    light0_soft: '#e4e4ea',
-    light1: '#d0d0d6',
-    light2: '#bcbcc2',
-    light3: '#a8a8ae',
-    light4: '#95959b',
-    light4_256: '#95959b',
-
-    bright_red: '#ff8a7f',
-    neutral_red: '#ff5c57',
-    faded_red: '#cb2a31',
-    bright_green: '#8effba',
-    neutral_green: '#5af78e',
-    faded_green: '#0cc864',
-    bright_yellow: '#ffffca',
-    neutral_yellow: '#f3f99d',
-    faded_yellow: '#c4cb72',
-    bright_blue: '#8cf5ff',
-    neutral_blue: '#57c7ff',
-    faded_blue: '#009bd1',
-    bright_orange: '#ffd892',
-    neutral_orange: '#fdab68',
-    faded_orange: '#cc803f',
-    bright_purple: '#ff98ef',
-    neutral_purple: '#ff6ac1',
-    faded_purple: '#ce3a95',
-    bright_aqua: '#c9ffff',
-    neutral_aqua: '#9aedfe',
-    faded_aqua: '#6cbfd0',
-    blue: '#458588',
-    magenta: '#b16286',
-    red: '#cc241d',
-    166: '#d65d0e'
+export const sw85 = {
+  neutrals: {
+    dark: {
+      dark0_hard: '#191b26',
+      dark0: '#282a36',
+      dark0_soft: '#383a46',
+      dark1: '#484a57',
+      dark2: '#595b68',
+      dark3: '#6b6d7a',
+      dark4: '#7d7f8c',
+    },
+    gray: {
+      gray_244: '#90929f',
+    },
+    light: {
+      light0: '#f9f9ff',
+      light0_soft: '#e4e4ea',
+      light1: '#d0d0d6',
+      light2: '#bcbcc2',
+      light3: '#a8a8ae',
+      light4: '#95959b',
+      light4_256: '#95959b',
+      light0_hard: '#ffffff',
+    },
+  },
+  bright: {
+    aqua: '#c9ffff',
+    blue: '#8cf5ff',
+    green: '#8effba',
+    orange: '#ffd892',
+    purple: '#ff98ef',
+    red: '#ff8a7f',
+    yellow: '#ffffca',
+  },
+  neutral: {
+    aqua: '#9aedfe',
+    blue: '#57c7ff',
+    green: '#5af78e',
+    orange: '#fdab68',
+    purple: '#ff6ac1',
+    red: '#ff5c57',
+    yellow: '#f3f99d',
+  },
+  faded: {
+    aqua: '#6cbfd0',
+    blue: '#009bd1',
+    green: '#0cc864',
+    orange: '#cc803f',
+    purple: '#ce3a95',
+    red: '#cb2a31',
+    yellow: '#c4cb72',
+  },
 }
+
+// const nord = {
+// --nord-deep-dark: #242931;
+// /*  polar night */
+// --nord0: #2E3440;
+// --nord1: #3B4252;
+// --nord2: #434C5E;
+// --nord3: #4C566A;
+// /* snow storm */
+// --nord4: #D8DEE9;
+// --nord5: #E5E9F0;
+// --nord6: #ECEFF4;
+// /* frost */
+// --nord7: #8FBCBB;
+// --nord8: #88C0D0;
+// --nord9: #81A1C1;
+// --nord10: #5E81AC;
+// /* aurora */
+// --nord11: #BF616A;
+// --nord12: #D08770;
+// --nord13: #EBCB8B;
+// --nord14: #A3BE8C;
+// --nord15: #B48EAD;
+// }
 
 
 
