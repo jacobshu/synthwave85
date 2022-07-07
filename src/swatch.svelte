d3543a216cb300e0c9f284f135db24b4f765709a
Jacob Shulenberger <jacob@builtdigital.io>
2022-07-07 15:49:47 -0500

    feat: add swatch component and class name generator

---
+++ src/swatch.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let bg;
+
+  function shouldBeDark(bg) {
+    if (bg.includes('400') || bg.includes('300') ||bg.includes('200') ||bg.includes('100')) {
+      return true
+    }
+    return false
+  }
+</script>
+<div class="px-8 py-16 rounded-lg {shouldBeDark(bg) ? 'text-gray' : 'text-white'} {bg}">{bg}</div>
