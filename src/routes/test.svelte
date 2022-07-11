17bdcb04807f26a79ca1283654d84edd84579030
Jacob Shulenberger <jacob@builtdigital.io>
2022-07-11 07:05:43 -0500

    wip: theme selector work

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,142 +1,201 @@
 <script>
-  let theme = 'sw';
-  $: keyword = theme === 'sw84' ? 'text-sw84-red-400' : 'text-sw85-red-400'
+  import ThemeSelector from '../theme-selector.svelte'
+
+  const themes = {
+    background: {
+      sw84: 'bg-sw84-neutral-600',
+      sw85: 'bg-sw85-neutral-600',
+      nord: 'bg-nord-neutral-600'
+    },
+    keyword: {
+      sw84: 'text-sw84-blue-400',
+      sw85: 'text-sw85-blue-600',
+      nord: 'text-nord-blue-400'
+    },
+    type: {
+      sw84: '',
+      sw85: '',
+      nord: ''
+    },
+    method: {
+      sw84: '',
+      sw85: '',
+      nord: ''
+    },
+    string: {
+      sw84: 'text-sw84-green-400',
+      sw85: 'text-sw85-green-400',
+      nord: 'text-nord--green-400'
+    },
+    container: {
+      sw84: '',
+      sw85: '',
+      nord: ''
+    },
+  }
+
+  let theme = 'sw84';
+  $: background = themes.background[theme]
+  $: keyword = themes.keyword[theme]
+  $: type = themes.type[theme]
+  $: method = themes.method[theme]
+  $: string = themes.string[theme]
+  $: container = themes.container[theme]
+
+
+  let buttons = "inline-flex items-center w-48 px-4 py-2 border border-transparent text-sm text-white font-medium justify-center rounded-md shadow-sm bg-vim-neutral-600"
+  let indent = "py-0.5 px-8"
+  let indent2 = "py-0.5 px-16"
+  let indent3 = "py-0.5 px-24"
 </script>
+<style>
+    p {
+        font-family: "Source Code Pro", sans-serif;
+        color: white;
+    }
+</style>
+<div class="flex flex-row-reverse p-12">
+  <div>
+    <div class="flex-column justify-center py-12">
+      <button class={buttons} on:click="{() => {theme = 'sw84'}}">synthwave84</button>
+      <button class={buttons} on:click="{() => {theme = 'sw85'}}">synthwave85</button>
+      <button class={buttons} on:click="{() => {theme = 'nord'}}">nord</button>
+    </div>
+    <div>
+      <ThemeSelector/>
+    </div>
+  </div>
 
-<button
-  on:click="{() => {theme = 'sw84'}}"
->synthwave84</button>
-<button
-  on:click="{() => {theme = 'sw85'}}"
->synthwave85</button>
-<button
-  on:click="{() => {theme = 'nord'}}"
->nord</button>
+  <section class="{background} flex items-start justify-start min-h-screen max-w-4xl p-16 mx-auto rounded-md">
+    <div class="px-4 py-5 sm:px-6">
+      <p><span class={keyword}>import</span> <span class={string}>'dart:async'</span>;</p>
+      <p><span class={keyword}>import</span> <span class={string}>'dart:io'</span>;</p>
+      <br/>
+      <p><span class={keyword}>class</span> <span class={type}>ApiProvider</span> &#123;</p>
+      <p class={indent}><span class={keyword}>final</span> <span class={type}>String</span><span
+        class={keyword}>?</span>
+        _baseUrl <span class={keyword}>=</span> dotenv.env<span class={container}>[</span>'SERVER'<span
+          class={container}>]</span>;</p>
 
-<p><span class="keyword">import</span> <span class="2">'dart:async'</span>;</p>
-<p><span class="keyword">import</span> <span class="2">'dart:io'</span>;</p>
+      <p class={indent}><span class={type}>Future</span>&#60;<span class={type}>dynamic</span>&#62; <span
+        class={method}>get</span><span class={container}>(</span><span class={type}>String</span> url<span
+        class={keyword}>,</span> params<span class={container}>)</span> <span class={keyword}>async</span> <span
+        class={container}>&#123;</span></p>
+      <p class={indent2}><span class={keyword}>if</span> (_port != null) iPort = int.tryParse(_port as String);</p>
 
-<p><span class="keyword">class</span> <span class="type">ApiProvider</span> &#123;</p>
-<p class="indent"><span class="keyword">final</span> <span class="type">String</span><span class="keyword">?</span>
-  _baseUrl <span class="keyword">=</span> dotenv.env<span class="container">[</span>'SERVER'<span
-    class="container">]</span>;</p>
+      <!--  dynamic responseJson;-->
+      <!--  Uri uri = Uri(-->
+      <!--  scheme: 'https',-->
+      <!--  host: _baseUrl,-->
+      <!--  port: iPort,-->
+      <!--  path: url,-->
+      <!--  queryParameters: params);-->
+      <!--  Inspector.log('GET : $uri');-->
+      <!--  try &#123;-->
+      <!--  final response = await http.get(uri,-->
+      <!--  headers: &#123;HttpHeaders.authorizationHeader: 'Bearer $&#123;_token!&#125;'&#125;);-->
+      <!--  responseJson = _response(response);-->
+      <!--&#125; on SocketException &#123;-->
+      <!--  throw FetchDataException('No Internet connection');-->
+      <!--&#125;-->
+      <!--  return responseJson;-->
+      <!--&#125;-->
 
-<p class="indent"><span class="type">Future</span><<span class="type">dynamic</span>> <span
-  class="method">get</span><span class="container">(</span><span class="type">String</span> url<span
-  class="keyword">,</span> params<span class="container">)</span> <span class="keyword">async</span> <span
-  class="container">&#123;</span></p>
-<!--  if (_port != null) iPort = int.tryParse(_port as String);-->
+      <!--  Future<dynamic> post(String url, body) async &#123;-->
+      <!--  int? iPort;-->
+      <!--  if (_port != null) iPort = int.tryParse(_port as String);-->
+      <!--  dynamic responseJson;-->
+      <!--  Uri uri = Uri(-->
+      <!--  scheme: 'https',-->
+      <!--  host: _baseUrl,-->
+      <!--  port: iPort,-->
+      <!--  path: url,-->
+      <!--  );-->
+      <!--  Inspector.log('POST : $uri');-->
+      <!--  try &#123;-->
+      <!--  final response = await http.post(uri, body: json.encode(body), headers: &#123;-->
+      <!--  'Content-type': 'application/json',-->
+      <!--  HttpHeaders.authorizationHeader: 'Bearer $&#123;_token!&#125;'-->
+      <!--&#125;);-->
+      <!--  responseJson = _response(response);-->
+      <!--&#125; on SocketException &#123;-->
+      <!--  throw FetchDataException('No Internet connection');-->
+      <!--&#125;-->
+      <!--  return responseJson;-->
+      <!--&#125;-->
 
-<!--  dynamic responseJson;-->
-<!--  Uri uri = Uri(-->
-<!--  scheme: 'https',-->
-<!--  host: _baseUrl,-->
-<!--  port: iPort,-->
-<!--  path: url,-->
-<!--  queryParameters: params);-->
-<!--  Inspector.log('GET : $uri');-->
-<!--  try &#123;-->
-<!--  final response = await http.get(uri,-->
-<!--  headers: &#123;HttpHeaders.authorizationHeader: 'Bearer $&#123;_token!&#125;'&#125;);-->
-<!--  responseJson = _response(response);-->
-<!--&#125; on SocketException &#123;-->
-<!--  throw FetchDataException('No Internet connection');-->
-<!--&#125;-->
-<!--  return responseJson;-->
-<!--&#125;-->
+      <!--  Future<dynamic> getLineItemsByPurchaseOrder(String purchaseOrder) async &#123;-->
+      <!--  return await get('/po/line-items', &#123;'purchaseOrderNumber': purchaseOrder&#125;);-->
+      <!--&#125;-->
 
-<!--  Future<dynamic> post(String url, body) async &#123;-->
-<!--  int? iPort;-->
-<!--  if (_port != null) iPort = int.tryParse(_port as String);-->
-<!--  dynamic responseJson;-->
-<!--  Uri uri = Uri(-->
-<!--  scheme: 'https',-->
-<!--  host: _baseUrl,-->
-<!--  port: iPort,-->
-<!--  path: url,-->
-<!--  );-->
-<!--  Inspector.log('POST : $uri');-->
-<!--  try &#123;-->
-<!--  final response = await http.post(uri, body: json.encode(body), headers: &#123;-->
-<!--  'Content-type': 'application/json',-->
-<!--  HttpHeaders.authorizationHeader: 'Bearer $&#123;_token!&#125;'-->
-<!--&#125;);-->
-<!--  responseJson = _response(response);-->
-<!--&#125; on SocketException &#123;-->
-<!--  throw FetchDataException('No Internet connection');-->
-<!--&#125;-->
-<!--  return responseJson;-->
-<!--&#125;-->
+      <!--  Future<dynamic> getMatchedLineItems(String purchaseOrder) async &#123;-->
+      <!--  return await get(-->
+      <!--  '/so/matched-items', &#123;'purchaseOrderNumber': purchaseOrder&#125;);-->
+      <!--&#125;-->
 
-<!--  Future<dynamic> getLineItemsByPurchaseOrder(String purchaseOrder) async &#123;-->
-<!--  return await get('/po/line-items', &#123;'purchaseOrderNumber': purchaseOrder&#125;);-->
-<!--&#125;-->
+      <!--  Future<dynamic> getRecentPOs() async &#123;-->
+      <!--  return await get('/po', null);-->
+      <!--&#125;-->
 
-<!--  Future<dynamic> getMatchedLineItems(String purchaseOrder) async &#123;-->
-<!--  return await get(-->
-<!--  '/so/matched-items', &#123;'purchaseOrderNumber': purchaseOrder&#125;);-->
-<!--&#125;-->
+      <!--  Future<dynamic> createPackage(package) async &#123;-->
+      <!--  return await post('/package', package);-->
+      <!--&#125;-->
 
-<!--  Future<dynamic> getRecentPOs() async &#123;-->
-<!--  return await get('/po', null);-->
-<!--&#125;-->
+      <!--  Future<dynamic> receiveStock(package) async &#123;-->
+      <!--  return await post('po/receive', package);-->
+      <!--&#125;-->
 
-<!--  Future<dynamic> createPackage(package) async &#123;-->
-<!--  return await post('/package', package);-->
-<!--&#125;-->
+      <!--  Future<dynamic> sendImageForScan(File imageFile) async &#123;-->
+      <!--  Inspector.wtf('sending image', imageFile);-->
+      <!--  dynamic responseJson;-->
+      <!--  int? iPort;-->
+      <!--  if (_port != null) iPort = int.tryParse(_port as String);-->
 
-<!--  Future<dynamic> receiveStock(package) async &#123;-->
-<!--  return await post('po/receive', package);-->
-<!--&#125;-->
+      <!--  final imageBytes = await imageFile.readAsBytes();-->
+      <!--  Uri uri = Uri(-->
+      <!--  scheme: 'https',-->
+      <!--  host: _baseUrl,-->
+      <!--  port: iPort,-->
+      <!--  path: 'scan/image',-->
+      <!--  );-->
 
-<!--  Future<dynamic> sendImageForScan(File imageFile) async &#123;-->
-<!--  Inspector.wtf('sending image', imageFile);-->
-<!--  dynamic responseJson;-->
-<!--  int? iPort;-->
-<!--  if (_port != null) iPort = int.tryParse(_port as String);-->
+      <!--  Inspector.log('POST : $uri');-->
+      <!--  int now = DateTime-->
+      <!--  .now()-->
+      <!--  .millisecondsSinceEpoch;-->
+      <!--  try &#123;-->
+      <!--  var request = http.MultipartRequest('POST', uri)-->
+      <!--  ..files.add(http.MultipartFile.fromBytes('label', imageBytes,-->
+      <!--  filename: '$now-scanned-label'));-->
+      <!--  var streamedResponse = await request.send();-->
+      <!--  var response = await http.Response.fromStream(streamedResponse);-->
+      <!--  responseJson = _response(response);-->
+      <!--&#125; on SocketException &#123;-->
+      <!--  throw FetchDataException('No Internet connection');-->
+      <!--&#125;-->
+      <!--  return responseJson;-->
+      <!--&#125;-->
 
-<!--  final imageBytes = await imageFile.readAsBytes();-->
-<!--  Uri uri = Uri(-->
-<!--  scheme: 'https',-->
-<!--  host: _baseUrl,-->
-<!--  port: iPort,-->
-<!--  path: 'scan/image',-->
-<!--  );-->
+      <!--  dynamic _response(http.Response response) &#123;-->
+      <!--  switch (response.statusCode) &#123;-->
+      <!--  case 200:-->
+      <!--  var responseJson = json.decode(response.body);-->
+      <!--  return responseJson;-->
+      <!--  case 400:-->
+      <!--  throw BadRequestException(response.body.toString());-->
+      <!--  case 401:-->
 
-<!--  Inspector.log('POST : $uri');-->
-<!--  int now = DateTime-->
-<!--  .now()-->
-<!--  .millisecondsSinceEpoch;-->
-<!--  try &#123;-->
-<!--  var request = http.MultipartRequest('POST', uri)-->
-<!--  ..files.add(http.MultipartFile.fromBytes('label', imageBytes,-->
-<!--  filename: '$now-scanned-label'));-->
-<!--  var streamedResponse = await request.send();-->
-<!--  var response = await http.Response.fromStream(streamedResponse);-->
-<!--  responseJson = _response(response);-->
-<!--&#125; on SocketException &#123;-->
-<!--  throw FetchDataException('No Internet connection');-->
-<!--&#125;-->
-<!--  return responseJson;-->
-<!--&#125;-->
-
-<!--  dynamic _response(http.Response response) &#123;-->
-<!--  switch (response.statusCode) &#123;-->
-<!--  case 200:-->
-<!--  var responseJson = json.decode(response.body);-->
-<!--  return responseJson;-->
-<!--  case 400:-->
-<!--  throw BadRequestException(response.body.toString());-->
-<!--  case 401:-->
-
-<!--  case 403:-->
-<!--  throw UnauthorisedException(response.body.toString());-->
-<!--  case 500:-->
-<!--  throw ServerException('Error from the server : $&#123;response.statusCode&#125;');-->
-<!--  default:-->
-<!--  throw FetchDataException(-->
-<!--  'Error occured during communication with the server with status code : $&#123;response-->
-<!--  .statusCode&#125;');-->
-<!--&#125;-->
-<!--&#125;-->
-<!--&#125;-->
+      <!--  case 403:-->
+      <!--  throw UnauthorisedException(response.body.toString());-->
+      <!--  case 500:-->
+      <!--  throw ServerException('Error from the server : $&#123;response.statusCode&#125;');-->
+      <!--  default:-->
+      <!--  throw FetchDataException(-->
+      <!--  'Error occured during communication with the server with status code : $&#123;response-->
+      <!--  .statusCode&#125;');-->
+      <!--&#125;-->
+      <!--&#125;-->
+      <!--&#125;-->
+    </div>
+  </section>
+</div>
---
+++ src/theme-selector.svelte
@@ -0,0 +1,54 @@
+<script>
+  import theme from '../colors/colors.cjs'
+  let element = 'keyword'
+  let elements = [
+    {name: 'keyword', color: '', shade: 500}
+  ]
+  $: bgColor = theme[element][shade]
+  $: shade = 500
+
+  function getColor(tw) {
+    let [theme, color, shade] = tw.split('-')
+    return {themedColor: `${theme}-${color}`, shade: shade}
+  }
+
+</script>
+<style >
+    input[type="range"] {
+        -webkit-appearance: none;
+        appearance: none;
+        background: transparent;
+        cursor: pointer;
+    }
+    input[type="range"]::-webkit-slider-runnable-track {
+        background: red;
+        height: 0.5rem;
+        border-radius: 5px;
+    }
+    input[type="range"]::-webkit-slider-thumb {
+        -webkit-appearance: none; /* Override default look */
+        appearance: none;
+        margin-top: -4px; /* Centers thumb on the track */
+        background-color: #fff;
+        height: 1rem;
+        width: 1rem;
+        border-radius: 50%;
+        border: 2px solid gray;
+    }
+
+</style>
+<div class="w-1/2">
+    <label for={element} class="font-bold text-gray-600">{element}</label>
+    <input id={element} type="range" list="tickmarks" step="100" bind:value={shade} min="100" max="900" class="w-full range ">
+    <datalist id="tickmarks" class="">
+      <option value="100" class=""></option>
+      <option value="200"></option>
+      <option value="300"></option>
+      <option value="400"></option>
+      <option value="500"></option>
+      <option value="600"></option>
+      <option value="700"></option>
+      <option value="800"></option>
+      <option value="900"></option>
+    </datalist>
+  </div>
